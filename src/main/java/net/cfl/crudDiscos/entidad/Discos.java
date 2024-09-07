cf787839b7503c3058f0ed89015ada53141071f1
maria sanchez <mariaisabelsanchez481@gmail.com>
2024-09-07 18:02:36 -0300

    Establecimos el campo Id com llave primaria, generamos el auto incremento de la llave primaria y indicamos los nombres de los campos en la base de datos

--- src/main/java/net/cfl/crudDiscos/entidad/Discos.java
+++ src/main/java/net/cfl/crudDiscos/entidad/Discos.java
@@ -1,6 +1,10 @@
 package net.cfl.crudDiscos.entidad;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,14 +15,24 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "tb_discos")
-@Id
-@GeneratedValues(estrategue = generetiontime.id)
+
+
+@Entity  //Esta notacion le dice a java que esta clase es una entidad
+@Table(name = "tb_discos") //Indica el nombre de la tabla en la base de datos
+/*CREAMOS LA CLASE Discos PARA JPA
+ * 
+ */
 public class Discos {
-	private Long id ;
+	@Id //Establecemos el campo id como PRIMARY KEY
+	@GeneratedValue(strategy = GenerationType.IDENTITY)// Generamos el auto increment
+	private Long id;
+	//La notacion @Column indica el nombre del campo en la base de datos
+	@Column(name = "d_titulo")
 	private String titulo;
+	@Column(name = "d_artista")
 	private String artista;
+	@Column(name = "d_duracion")
 	private int duracion;
+	@Column(name = "d_cod", nullable = false, unique = true)
 	private String cod;
 }
