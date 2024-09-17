9a5a939906d4bb68fa0c1499b3e92461d223f66d
Lucas Soria <lucassoria624@gmail.com>
2024-09-17 19:26:58 -0300

    Agregando a la clase DiscoServicioImp las anotaciones @AllArgsConstructor y @Service

--- src/main/java/net/cfl/crudDiscos/servicios/DiscoServicio.java
+++ src/main/java/net/cfl/crudDiscos/servicios/DiscoServicio.java
@@ -2,7 +2,9 @@ package net.cfl.crudDiscos.servicios;
 
 import net.cfl.crudDiscos.dto.DiscoDto;
 
+
 public interface DiscoServicio {
+	
 	
 	DiscoDto crearDisco(DiscoDto discoDto);
 	
--- src/main/java/net/cfl/crudDiscos/servicios/imp/DiscoServicioImp.java
+++ src/main/java/net/cfl/crudDiscos/servicios/imp/DiscoServicioImp.java
@@ -1,11 +1,16 @@
 package net.cfl.crudDiscos.servicios.imp;
 
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
 import net.cfl.crudDiscos.dto.DiscoDto;
 import net.cfl.crudDiscos.entidad.Discos;
 import net.cfl.crudDiscos.mapper.DiscoMapper;
 import net.cfl.crudDiscos.repositorio.DiscoRepositorio;
 import net.cfl.crudDiscos.servicios.DiscoServicio;
 
+@Service
+@AllArgsConstructor
 public class DiscoServicioImp implements DiscoServicio{
 	
 	private DiscoRepositorio discoRepositorio;
@@ -13,7 +18,7 @@ public class DiscoServicioImp implements DiscoServicio{
 	@Override
 	public DiscoDto crearDisco(DiscoDto discoDto) {
 		Discos disco = DiscoMapper.mapToDisco(discoDto);
-		//
+		//El metodo .save() de JPA guarda los datos en la base de datos
 		Discos discoGuardado = discoRepositorio.save(disco);
 		return DiscoMapper.mapToDiscoDto(discoGuardado);
 	}
